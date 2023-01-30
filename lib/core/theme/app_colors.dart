522eba01bfd331a63a37f8fa4f90916db44f8468
Amarkumar Belkhede <belkhedeamar@gmail.com>
2023-01-30 17:06:57 +0530

    some changes

--- lib/core/theme/app_colors.dart
+++ lib/core/theme/app_colors.dart
@@ -5,4 +5,21 @@ class AppColors {
   static const Color lightGrey = Color(0xFFE0E0E0);
   static const Color transparent = Color(0x00000000);
   static const Color sandyColor = Color(0xFFA48322);
+
+  
+  static const MaterialColor mms_color = MaterialColor(
+    0xFF193785,
+    <int, Color>{
+      50: Color(0xFF9BB5F7),
+      100: Color(0xFF829EE3),
+      200: Color(0xFF6585D4),
+      300: Color(0xFF4B6DC2),
+      400: Color(0xFF4363B3),
+      500: Color(0xFF2B4DA1),
+      600: Color(0xFF193785),
+      700: Color(0xFF102F7D),
+      800: Color(0xFF0A2876),
+      900: Color(0xFF051F63),
+    },
+  );
 }
--- lib/my_app.dart
+++ lib/my_app.dart
@@ -31,7 +31,7 @@ class MyApp extends StatelessWidget {
 
           title: 'Flutter Demo',
           theme: ThemeData(
-            primarySwatch: Colors.indigo,
+            primarySwatch: AppColors.mms_color,
           ),
           // home: const MyHomePage(title: 'Flutter Demo Home Page'),
         ),
